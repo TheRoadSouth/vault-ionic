7fd6ba3ce8b8ef742a13a43e8c0eca4cb5f69fa9
Martin Przybyla <mprzybyla@ancestry.com>
2015-02-23 13:49:03 -0800

    more work on basic structure

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'vault.controllers', 'vault.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -50,7 +50,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     .state('tab.videoupload', {
       url: '/videoupload',
       views: {
-        'tab-videos': {
+        'video-upload': {
           templateUrl: 'templates/video-upload.html',
           controller: 'VideoUploadCtrl'
         }
