1f6e03db1e97f9f735082ab6b19987876aa9985a
Yukiko Añonuevo <kiroin@gmail.com>
2015-02-25 13:59:59 -0800

    Cleaning up git platforms

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Applications/XAMPP/xamppfiles/htdocs/vault-ionic/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Applications/XAMPP/xamppfiles/htdocs/vault-ionic/platforms/android/CordovaLib/AndroidManifest.xml \
--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Applications/XAMPP/xamppfiles/htdocs/vault-ionic/platforms/android/ant-build/AndroidManifest.xml : \
-/Applications/XAMPP/xamppfiles/htdocs/vault-ionic/platforms/android/AndroidManifest.xml \
